816648ac2f545e00f433441edd4acff7bd1e374e
sugyan <sugi1982@gmail.com>
2025-07-27 23:31:11 +0900

    update

--- camera_running_debug.swift
+++ camera_running_debug.swift
@@ -1,80 +1,84 @@
 // 必要なフレームワークをインポート
+
 import Foundation
 import CoreMediaIO
 
 
 // システムオブジェクトIDを取得
-let systemObjectID = CMIOObjectID(kCMIOObjectSystemObject)
 // デバイスリストのサイズを格納する変数
+let systemObjectID = CMIOObjectID(kCMIOObjectSystemObject)
 var dataSize: UInt32 = 0
 
 
 // デバイスリスト取得用のプロパティアドレスを設定
 var deviceListAddress = CMIOObjectPropertyAddress(
-    mSelector: UInt32(kCMIOHardwarePropertyDevices), // デバイス一覧
-    mScope: UInt32(kCMIOObjectPropertyScopeGlobal),  // グローバルスコープ
-    mElement: UInt32(kCMIOObjectPropertyElementMain) // メイン要素
+    mSelector: UInt32(kCMIOHardwarePropertyDevices),
+    mScope: UInt32(kCMIOObjectPropertyScopeGlobal),
+    mElement: UInt32(kCMIOObjectPropertyElementMain)
 )
 
 
 // デバイスリストのサイズを取得
-let status1 = CMIOObjectGetPropertyDataSize(systemObjectID, &deviceListAddress, 0, nil, &dataSize)
-guard status1 == noErr else {
-    print("Failed to get device list size.") // 失敗時
+guard CMIOObjectGetPropertyDataSize(systemObjectID, &deviceListAddress, 0, nil, &dataSize) == noErr else {
+    print("Failed to get device list size.")
     exit(1)
 }
 
 
 // デバイス数を計算し、ID格納用配列を用意
+// 実際に使用されたデータサイズ
 let numberOfDevices = Int(dataSize) / MemoryLayout<CMIOObjectID>.size
 var deviceIDs = [CMIOObjectID](repeating: 0, count: numberOfDevices)
-// 実際に使用されたデータサイズ
 var dataUsed: UInt32 = 0
 
 
 // デバイスID一覧を取得
-let status2 = CMIOObjectGetPropertyData(systemObjectID, &deviceListAddress, 0, nil, dataSize, &dataUsed, &deviceIDs)
-guard status2 == noErr else {
-    print("Failed to get device list.") // 失敗時
+guard CMIOObjectGetPropertyData(systemObjectID, &deviceListAddress, 0, nil, dataSize, &dataUsed, &deviceIDs) == noErr else {
+    print("Failed to get device list.")
     exit(1)
 }
 
 
 // デバイス一覧を表示
-print("Device List: (numberOfDevices=\(numberOfDevices))")
-for deviceID in deviceIDs {
-    // デバイス名取得用のプロパティアドレスを設定
-    var nameAddress = CMIOObjectPropertyAddress(
-        mSelector: UInt32(kCMIOObjectPropertyName), // デバイス名
-        mScope: UInt32(kCMIOObjectPropertyScopeGlobal),
-        mElement: UInt32(kCMIOObjectPropertyElementMain)
-    )
-    // デバイス名を格納する変数
-    var deviceName: Unmanaged<CFString>?
-    let nameSize = UInt32(MemoryLayout<CFString?>.size)
-    var nameUsed: UInt32 = 0
-    // デバイス名を取得
-    let nameStatus = CMIOObjectGetPropertyData(deviceID, &nameAddress, 0, nil, nameSize, &nameUsed, &deviceName)
-    let nameStr: String
-    if nameStatus == noErr, let cfStr = deviceName?.takeRetainedValue() {
-        nameStr = cfStr as String
-    } else {
-        nameStr = "(Name not available)" // 取得失敗時
+
+
+func isAnyCameraRunning(deviceIDs: [CMIOObjectID]) -> (Bool, String?) {
+    for deviceID in deviceIDs {
+        var nameAddress = CMIOObjectPropertyAddress(
+            mSelector: UInt32(kCMIOObjectPropertyName),
+            mScope: UInt32(kCMIOObjectPropertyScopeGlobal),
+            mElement: UInt32(kCMIOObjectPropertyElementMain)
+        )
+        var deviceName: Unmanaged<CFString>?
+        var nameUsed: UInt32 = 0
+        let nameStatus = CMIOObjectGetPropertyData(deviceID, &nameAddress, 0, nil, UInt32(MemoryLayout<CFString?>.size), &nameUsed, &deviceName)
+        let nameStr = (nameStatus == noErr && deviceName != nil) ? deviceName!.takeRetainedValue() as String : "(Name not available)"
+
+        var isRunningAddress = CMIOObjectPropertyAddress(
+            mSelector: UInt32(kCMIODevicePropertyDeviceIsRunningSomewhere),
+            mScope: UInt32(kCMIOObjectPropertyScopeGlobal),
+            mElement: UInt32(kCMIOObjectPropertyElementMain)
+        )
+        var isRunning: UInt32 = 0
+        var isRunningUsed: UInt32 = 0
+        let isRunningStatus = CMIOObjectGetPropertyData(deviceID, &isRunningAddress, 0, nil, UInt32(MemoryLayout<UInt32>.size), &isRunningUsed, &isRunning)
+        if isRunningStatus == noErr && isRunning != 0 {
+            return (true, nameStr)
+        }
     }
+    return (false, nil)
+}
 
-    // デバイスが使用中かどうかのプロパティアドレスを設定
-    var isRunningAddress = CMIOObjectPropertyAddress(
-        mSelector: UInt32(kCMIODevicePropertyDeviceIsRunningSomewhere), // 使用中判定
-        mScope: UInt32(kCMIOObjectPropertyScopeGlobal),
-        mElement: UInt32(kCMIOObjectPropertyElementMain)
-    )
-    // 使用中かどうかの値を格納する変数
-    var isRunning: UInt32 = 0
-    let isRunningSize = UInt32(MemoryLayout<UInt32>.size)
-    var isRunningUsed: UInt32 = 0
-    // デバイスが使用中かどうかを取得
-    let isRunningStatus = CMIOObjectGetPropertyData(deviceID, &isRunningAddress, 0, nil, isRunningSize, &isRunningUsed, &isRunning)
-
-    // デバイスID・名前・使用中かどうかを表示
-    print("Device ID \(deviceID): \(nameStr) | isRunningSomewhere=\(isRunning) (status=\(isRunningStatus))")
+var lastState: Bool? = nil
+while true {
+    let (isRunning, runningName) = isAnyCameraRunning(deviceIDs: deviceIDs)
+    if lastState == nil || lastState != isRunning {
+        if isRunning {
+            print("カメラ使用中になりました: \(runningName ?? "Unknown")")
+        } else {
+            print("カメラが全て未使用になりました")
+        }
+        lastState = isRunning
+    }
+    sleep(1)
 }
